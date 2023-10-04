f2f64188082ebbd06b748984da01be42e9f1b2a8
Cristian Gonzalez <dotcomapunto@gmail.com>
2023-10-04 04:56:51 -0700

    Git Init

---
+++ 13_Schedule Task.ps1
@@ -0,0 +1,13 @@
+﻿
+# WO Display Auto Start
+$woPath = "C:\WATCHOUT 6"
+
+$action = New-ScheduledTaskAction -Execute $woPath\WATCHPOINT.exe -WorkingDirectory $woPath
+
+$trigger =  New-ScheduledTaskTrigger -AtLogOn
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "WATCHPOINT" -Description "Run Watchpoint at login" -RunLevel Highest -Force 
+
+
+
+Read-Host -Prompt "Press Enter to exit"
